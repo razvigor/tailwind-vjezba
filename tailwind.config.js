dff6e3c480f42b89f9596fcdfdadffe799156bb0
razvigor <kamaradsasa@gmail.com>
2024-07-17 14:37:13 +0200

    style and config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   content: ['./public/*.html'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        widescreen: { raw: '(min-aspect-ratio: 3/2)' },
+        tallscreen: { raw: '(max-aspect-ratio: 13/20)' },
+      },
+    },
   },
   plugins: [],
 };
